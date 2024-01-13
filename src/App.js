259dd6309cee7bd5f325bd44c29a979ffdbf6566
lindijor <jorgensenlindis@gmail.com>
2024-01-13 22:25:20 +0100

    Added all visible elements and CSS styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weatherAppBody">
+      <form>
+        <input
+          type="search"
+          placeholder="Type a city..."
+          className="searchBar"
+        ></input>
+        <input type="submit" value="Search" className="searchBarButton"></input>
+      </form>
+      <div className="weatherContainer">
+        <div className="currentTemperature">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="weather-icon"
+            width={64}
+            height={64}
+          />
+          <p className="degrees">-4</p>
+          <p className="celsius">°C</p>
+        </div>
+
+        <ul className="weatherDescription">
+          <li>Nedbør: 0%</li>
+          <li>Luftfuktighet: 76%</li>
+          <li>Vind: 1 m/s</li>
+        </ul>
+
+        <ul className="dateDescription">
+          <h2>Været</h2>
+          <li>lørdag kl 21:00</li>
+          <li>For det meste skyet</li>
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
